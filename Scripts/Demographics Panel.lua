750d8943304a8ce0a6ff8ea34ed13280260f513a
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-10-28 05:40:03 -0700

    functional population fields

--- Scripts/Demographics Panel.lua
+++ Scripts/Demographics Panel.lua
@@ -6,6 +6,8 @@ include("InstanceManager");
 include("SupportFunctions");
 include("CitySupport");
 
+local human_id = nil
+
 -- ADD IS EVER ALIVE TO PLAYER BEFORE FUNCITON CALL
 --[[Get total population of player's empire.
 	return total population]]
@@ -35,7 +37,7 @@ local function GetDemographics()
 			if(p ~= nil) then
 				local pop = GetPop(p)
 				pop = math.ceil(pop)
-				if p:GetID() >= 0 then
+				if p:GetID() >= 0 and p:IsAlive() then
 					demographics[p:GetID()] = pop
 				end
 
@@ -45,12 +47,24 @@ local function GetDemographics()
 	return demographics
 end
 
+local function GetCropYield(player)
+	-- get crop yield of player
+end
+
+local function GetLand(player)
+	-- get total land of player
+end
+
+local function GetGNP(player)
+	-- get gnp of player
+end
+
 local count = 0;
 local players = 30;
 
 --[[Create demographics through GetDemographics function
 	Iterate through table and print player id and total population]]
-function ConditionalPrintPop()
+function UpdatePopulation()
 	local demographics = nil
 	if count >= players then
 				-- Temporary way to trigger refresh
@@ -63,22 +77,70 @@ function ConditionalPrintPop()
 end
 
 --[[Add custom function to fire when PlayerTurnStarted event fires]]
-function PlayerTurnStarted()
-	local context = ContextPtr
-	local control = Controls
-	if context ~= nil then
-		ContextPtr:LookUpControl("message"):SetText("MESSAGE HERE")
+function UpdatePanel()
+	print("getting demographics")
+	local demographics = GetDemographics()
+	print("creating string")
+	local pop_text :string = ""
+	local gnp_text	:string = ""
+	local land_text :string = ""
+	local crop_yield :string = ""
+	local rank = 1
+	local average = 0
+	local worst = 0
+	local best = 0
+	local count = 0
+	-- get and set population value
+	pop_text = pop_text .. demographics[human_id]-- get democraphic of player. Currently incorrect. Need to get ID of active player
+	Controls.pop_value:SetText(pop_text)
+	local tmp = demographics[human_id]
+	best = tmp
+	worst = tmp
+	for i, j in pairs(demographics) do
+		if i >= 0 then
+			if j > tmp then rank = rank + 1 end
+			if j < worst then worst = j end
+			if j > best then best = j end
+			average = average + j
+			count = count + 1
+		end
 	end
-	GameEvents.PlayerTurnStarted.Add(ConditionalPrintPop)
+
+	pop_text = ""
+	pop_text = pop_text .. rank
+	print("setting rank: ", rank)
+	Controls.pop_rank:SetText(pop_text)
+
+	pop_text = ""
+	pop_text = pop_text .. worst
+	print("setting worst: ", worst)
+	Controls.pop_worst:SetText(pop_text)
+
+	pop_text = ""
+	pop_text = pop_text .. best
+	print("setting best: ", best)
+	Controls.pop_best:SetText(pop_text)
+
+	pop_text = ""
+	pop_text = pop_text .. math.ceil(average / count)
+	print("setting average: ", pop_text)
+	Controls.pop_average:SetText(pop_text)
 end
 
-GameEvents.PlayerTurnStarted.Add(PlayerTurnStarted)
+--GameEvents.PlayerTurnStarted.Add(PlayerTurnStarted)
 
 function OnOK()
-	Controls.D_BOX:SetHide(true) --Change to destroy?
+	--Controls.D_BOX:SetHide(true) --Change to destroy?
+	UpdatePanel()
 end
 
+-- change in case of multiplayers or hotseat
 function RigOkButton()
+	for i, j in pairs(Players) do
+		if j then
+			if j:IsHuman() then human_id = j:GetID() break end
+		end
+	end
 	Controls.OK:RegisterCallback(Mouse.eLClick, OnOK)
 end
 
